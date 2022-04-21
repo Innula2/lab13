d9e3d2335b07019e40fe5249c2453345ffc8cd57
innka <innka@INNUSIK>
2022-04-22 01:06:39 +0300

    Добавьте файлы проекта.

---
+++ ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace lab13
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;" +
+            "Port=5433;" +
+            "Database=testdb;" +
+            "Username=postgres;" +
+            "Password=rinh2021");
+        }
+    }
+
+}
